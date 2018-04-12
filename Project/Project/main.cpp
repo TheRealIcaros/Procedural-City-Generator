f675ec2a4cf6f96263dadc3da133b5d2d16daff8
adbr14 <bradstar95@gmail.com>
2018-04-12 17:27:38 +0200

    Graphic pipline does now work!!!

--- Project/Project/main.cpp
+++ Project/Project/main.cpp
@@ -91,7 +91,7 @@ int main()
 		return -1;
 	}
 
-	renderPass.createShader("Graphic/Shaders/vertexShader", "NULL", "../Graphic/Shaders/fragmentShader");
+	renderPass.createShader("Graphic/Shaders/vertexShader", "NULL", "Graphic/Shaders/fragmentShader");
 	createTriangleData();
 
 	while (!glfwWindowShouldClose(window))
@@ -115,7 +115,7 @@ int main()
 void initiateGLFW()
 {
 	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 }
@@ -182,21 +182,39 @@ void keyInput(GLFWwindow* window)
 void createTriangleData()
 {
 	float vertices[] = {
+
 		-0.5f, -0.5f, 0.0f,
 		 0.5f, -0.5f, 0.0f,
 		 0.0f,  0.5f, 0.0f
 	};
 
+	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+	glBindVertexArray(VAO);
+
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+	// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+	glBindVertexArray(0);
 }
 
 void render()
 {
 	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-	
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	// draw our first triangle
+	glUseProgram(renderPass.getShaderProgramID());
+	glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+	glDrawArrays(GL_TRIANGLES, 0, 3);
 	
 }
