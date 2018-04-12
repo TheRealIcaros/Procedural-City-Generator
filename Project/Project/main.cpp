de1a1daa389cc07582e5f38c14cfc4b5dde86f75
adbr14 <bradstar95@gmail.com>
2018-04-12 10:06:40 +0200

    start of the project

--- Project/Project/main.cpp
+++ Project/Project/main.cpp
@@ -1,12 +1,89 @@
+#include <glad/glad.h>
+#include <GLFW\glfw3.h>
 #include <iostream>
+
+//Defines
+#include "Defines.h"
+
+void initiateGLFW();
+void resizeWindow(GLFWwindow* window, int width, int height);
+bool initiateWindow(GLFWwindow* window);
+bool startSequence(GLFWwindow* window);
+
 
 int main()
 {
+	initiateGLFW();
+	
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", NULL, NULL);;
 
-	std::cout << "Test text Test" << std::endl;
+	if (startSequence(window) == false)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
 
 	std::system("PAUSE");
 	//Kaffe
 
+
+	glfwTerminate();
 	return 0;
+}
+
+void initiateGLFW()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+}
+
+
+void resizeWindow(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+bool initiateWindow(GLFWwindow* window)
+{
+	bool returnValue = true;
+	
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		returnValue = false;
+	}
+	
+
+	return returnValue;
+}
+
+bool startSequence(GLFWwindow* window)
+{
+	bool returnValue = true;
+
+	if (initiateWindow(window) == false)
+		returnValue = false;
+
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		returnValue = false;
+	}
+
+	glViewport(0, 0, 800, 600);
+
+	glfwSetFramebufferSizeCallback(window, resizeWindow);
+
+	return returnValue;
 }
