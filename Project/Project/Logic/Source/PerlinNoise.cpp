09bed941e84b8bdaa75eb1641318f5049c79ceab
dreas94 <dreas94@gmail.com>
2018-04-16 18:52:36 +0200

    tried to work on perlin noise

--- Project/Project/Logic/Source/PerlinNoise.cpp
+++ Project/Project/Logic/Source/PerlinNoise.cpp
@@ -1,4 +1,7 @@
 #include "..\header\PerlinNoise.h"
+#include <random>
+#include <numeric>
+
 
 PerlinNoise::PerlinNoise()
 {
@@ -6,17 +9,41 @@ PerlinNoise::PerlinNoise()
 
 PerlinNoise::PerlinNoise(unsigned int seed)
 {
-	p.resize(256);
+	pn.resize(256);
 
 	// Fill p with values from 0 to 255
-	std::iota(p.begin(), p.end(), 0);
+	std::iota(pn.begin(), pn.end(), 0);
 
 	// Initialize a random engine with seed
+	std::default_random_engine engine(seed);
+
+	// Suffle  using the above random engine
+	std::shuffle(pn.begin(), pn.end(), engine);
+
+	// Duplicate the permutation vector
+	pn.insert(pn.end(), pn.begin(), pn.end());
 }
 
-double PerlinNoise::noise(double x, double y, double z)
+double PerlinNoise::noise(double x, double y)
 {
-	return 0.0;
+	//Find the square that contains the point
+	int X = (int)floor(x) & 255;
+	int Y = (int)floor(y) & 255;
+
+	//Find the relative x, y point in the square
+	x -= floor(x);
+	y -= floor(y);
+
+	//Alter the values with fade for smoother
+	double u = fade(x);
+	double v = fade(y);
+
+	// Hash coordinates of the 4 square corners
+	
+
+	double res;
+
+	return (res + 1.0) / 2.0;
 }
 
 double PerlinNoise::fade(double t)
@@ -29,7 +56,7 @@ double PerlinNoise::lerp(double t, double a, double b)
 	return 0.0;
 }
 
-double PerlinNoise::grad(int hash, double x, double y, double z)
+double PerlinNoise::grad(int hash, double x, double y)
 {
 	return 0.0;
 }
--- Project/Project/Logic/header/PerlinNoise.h
+++ Project/Project/Logic/header/PerlinNoise.h
@@ -1,22 +1,22 @@
 #ifndef PERLINNOISE_H
 #define PERLINNOISE_H
 #include <vector>;
-#include <numeric>
 
 class PerlinNoise
 {
-	std::vector<int> p;
+	std::vector<int> pn;
 
 private:
 	double fade(double t);
+	//Linear Interpolation
 	double lerp(double t, double a, double b);
-	double grad(int hash, double x, double y, double z);
+	double grad(int hash, double x, double y);
 public:
 	// Initialize with the reference values for the permutation vector
 	PerlinNoise();
 	// Generate a new permutation vector based on the value of seed
 	PerlinNoise(unsigned int seed);
-	// Get a noise value, for 2D images z can have any value
-	double noise(double x, double y, double z);
+	// Get a noise value, for 2D image
+	double noise(double x, double y);
 };
 #endif
