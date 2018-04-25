a0e3233fddaca3d3174c842f25b239f60760f6dc
adbr14 <bradstar95@gmail.com>
2018-04-25 19:24:33 +0200

    Assimp is included correctly. Need to implement the model loader.

--- Project/Project/Graphic/header/Model.h
+++ Project/Project/Graphic/header/Model.h
@@ -5,14 +5,31 @@
 #include "Mesh.h"
 
 //Other includes
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
 //add the assimp includes here...
 class Model
 {
 private:
+	struct Texture
+	{
+		unsigned int id;
+		std::string type;
+		std::string path;
+	};
+
+
+	std::vector<Texture> textures_loaded;
 	std::vector<Mesh> meshes;
 	std::string directory;
+	Assimp::Importer import;
 
 	void loadModel(std::string path);
+	void processNode(aiNode *node, const aiScene *scene);
+	Mesh processMesh(aiMesh *mesh, const aiScene *scene);
+	std::vector<Texture> loadMaterialTextures(aiMaterial *mat, aiTextureType type, std::string typeName);
 	//void processMesh(aiNode* node, const aiScene* scene);
 public:
 	Model();
